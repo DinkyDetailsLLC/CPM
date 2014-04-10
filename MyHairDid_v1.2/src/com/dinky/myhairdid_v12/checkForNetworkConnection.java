8315376c38ee91f95ebfe143676a1493e32d845a
Dan <Dan@Dinkydetails.com>
2014-04-10 09:01:16 -0400

    Setting up the network connection detector

---
+++ MyHairDid_v1.2/src/com/dinky/myhairdid_v12/checkForNetworkConnection.java
@@ -0,0 +1,31 @@
+package com.dinky.myhairdid_v12;
+
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+ 
+public class checkForNetworkConnection {
+     
+    private Context _context;
+     
+    public checkForNetworkConnection(Context context){
+        this._context = context;
+    }
+ 
+    public boolean isConnectingToInternet(){
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+          if (connectivity != null)
+          {
+              NetworkInfo[] info = connectivity.getAllNetworkInfo();
+              if (info != null)
+                  for (int i = 0; i < info.length; i++)
+                      if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                      {
+                          return true;
+                      }
+ 
+          }
+          return false;
+    }
+}
