a92ad04695b88643aebad65f9815b616fb38de2a
Dan <Dan@Dinkydetails.com>
2014-04-30 20:18:43 -0400

    firing back up production

--- myHairDid_iOS/myHairDid/DashboardViewController.m
+++ myHairDid_iOS/myHairDid/DashboardViewController.m
@@ -6,9 +6,29 @@
 //  Copyright (c) 2014 Dinky_Details. All rights reserved.
 //
 
+extern NSString *objectIdStr;
+NSString *selectedUser;
+extern NSString *nameStr;
 #import "DashboardViewController.h"
+#import <Parse/Parse.h>
+#import "MoreInfoViewController.h"
+#import "EditInfoViewController.h"
+#import "Utilities-1.h"
+#import "MBProgressHUD.h"
 
 @interface DashboardViewController ()
+
+{
+    
+    NSArray *tableArray;
+    NSMutableArray *dataArray1;
+    NSString *alerStr;
+    PFQuery *query;
+    NSInteger  selectedInd;
+    NSString *alertString;
+    NSInteger alertTag;
+    MBProgressHUD *HUD;
+}
 
 @end
 
@@ -22,11 +42,201 @@
     }
     return self;
 }
+-(void)viewWillAppear:(BOOL)animated
+{
+    alerStr=@"0";
+    
+    HUD=[MBProgressHUD showHUDAddedTo:self.view animated:YES];
 
+    // Adding the MBPRogressHUD on LoginButton
+
+    HUD.labelText=@"Please Wait";
+    
+    dataArray1=[[NSMutableArray alloc]init];
+    UIBarButtonItem *backButtonObject=[[UIBarButtonItem alloc]initWithTitle:@"Logout" style:UIBarButtonItemStylePlain target:self action:@selector(Logout)];
+    self.navigationItem.leftBarButtonItem=backButtonObject;
+    self.navigationItem.leftBarButtonItem.tintColor=[UIColor blackColor];
+    
+    
+    UIBarButtonItem *addButtonObject=[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(AddInfo)];
+    self.navigationItem.rightBarButtonItem=addButtonObject;
+    self.navigationItem.rightBarButtonItem.tintColor=[UIColor blackColor];
+    
+    query=[PFQuery queryWithClassName:@"AdditionalInfoDB"];
+    [query whereKey:@"Admin" equalTo:nameStr];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
+     {
+         if (!error)
+             
+         {
+             
+             
+             dataArray1 =[objects valueForKey:@"name"];
+             
+             if (dataArray1.count==0)
+                 
+             {
+                 alertString =@"Add Your Clients.";
+                 alertTag=5;
+                 [Utilities alertViewMethod:alertString :alertTag];
+                 HUD.hidden=YES;
+                 
+             }
+             [self.tableForName reloadData];
+         }
+     }];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.title=@"Clients";
+    self.view.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"background@2x"]];   // Setting the background image
+    
+    
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [dataArray1 count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    UILongPressGestureRecognizer *lpgr = [[UILongPressGestureRecognizer alloc]
+                                          initWithTarget:self action:@selector(handleLongPress:)];
+    
+    lpgr.minimumPressDuration = 1.0;
+    
+    [cell addGestureRecognizer:lpgr];
+    cell.backgroundColor=[UIColor clearColor];
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.textLabel.text=[dataArray1 objectAtIndex:indexPath.row];
+    HUD.hidden=YES;
+    return cell;
+    
+}
+- (float)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    // This will create a "invisible" footer
+    return 0.01f;
+}
+-(void)handleLongPress:(UILongPressGestureRecognizer *)gestureRecognizer
+{
+    CGPoint p = [gestureRecognizer locationInView:self.tableForName];
+    
+    NSIndexPath *indexPath = [self.tableForName indexPathForRowAtPoint:p];
+    if (indexPath == nil)
+        NSLog(@"long press on table view but not on a row");
+    else
+        NSLog(@"long press on table view at row %d", indexPath.row);
+    selectedInd=indexPath.row;
+    
+    if ([alerStr isEqual:@"1"])
+    {
+        NSLog(@"Repeat");
+    }
+    else
+    {
+        alerStr=@"1";
+        UIAlertView *DeleteAlert=[[UIAlertView alloc]initWithTitle:@"" message:@"Are you sure you want to delete?" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
+        
+        [DeleteAlert show];
+        DeleteAlert.tag=2;
+    }
+    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    selectedUser=[dataArray1 objectAtIndex:indexPath.row];
+    
+    EditInfoViewController *editInfo=[[EditInfoViewController alloc]initWithNibName:@"EditInfoViewController" bundle:Nil];
+    [self.navigationController pushViewController:editInfo animated:YES];
+}
+
+-(void)Logout
+{
+    UIAlertView *LogoutAlert=[[UIAlertView alloc]initWithTitle:@"" message:@"Are You Sure You Want To Logout?" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
+    [LogoutAlert show];
+    LogoutAlert.tag=1;
+}
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+    if (alertView.tag==1)
+    {
+        if (buttonIndex==0)
+        {
+            
+        }
+        else if (buttonIndex==1)
+            
+        {
+            [self.navigationController popViewControllerAnimated:YES];
+        }
+    }
+    
+    if (alertView.tag==2)
+    {
+        if (buttonIndex==0)
+        {
+            alerStr=@"0";
+            NSLog(@"No");
+            
+        }
+        else if (buttonIndex==1)
+        {
+            alerStr=@"0";
+            
+            query = [PFQuery queryWithClassName:@"AdditionalInfoDB"];
+            [query whereKey:@"name" equalTo:[dataArray1 objectAtIndex:selectedInd]];
+            
+            [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
+             
+             {
+                 if (!error)
+                 {
+                     // The find succeeded.
+                     // Do something with the found objects
+                     for (PFObject *object in objects)
+                     {
+                         
+                         [object deleteInBackground];
+                         NSLog(@"Deleted");
+                         [self viewWillAppear:YES];
+                         
+                         
+                     }
+                     
+                     [self.tableForName reloadData];
+                     
+                 }
+                 else
+                 {
+                     // Log details of the failure
+                     NSLog(@"Error: %@ %@", error, [error userInfo]);
+                 }
+             }];
+            
+        }
+    }
+    
+}
+-(void)AddInfo
+{
+    
+    MoreInfoViewController *moreInfo=[[MoreInfoViewController alloc]initWithNibName:@"MoreInfoViewController" bundle:Nil];
+    [self.navigationController pushViewController:moreInfo animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,3 +246,4 @@
 }
 
 @end
+
--- myHairDid_iOS/myHairDid/SignUPViewController.m
+++ myHairDid_iOS/myHairDid/SignUPViewController.m
@@ -7,9 +7,20 @@
 //
 
 #import "SignUPViewController.h"
+#import <Parse/Parse.h>
+#import "ViewController.h"
+#import "MBProgressHUD.h"
+#import "Utilities-1.h"
 
 @interface SignUPViewController ()
-
+{
+    
+    PFObject *testObject;
+    MBProgressHUD *HUD;
+    NSString *alertString;
+    NSInteger alertTag;
+    
+}
 @end
 
 @implementation SignUPViewController
@@ -27,6 +38,12 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.title=@"Register";
+    
+    
+    self.view.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"background@2x"]];    // Setting the background image
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +51,105 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)registerBtn:(id)sender
+{
+    
+    /////////// Empty TextField Validation ///////////////
+    
+    if ([self.nameSignUp.text isEqual:@""] || [self.passwordSignUp.text isEqual:@""]  || [self.emailSignUp.text isEqual:@""] )
+        
+    {
+        alertString =@"Please Enter All The Details.";
+        alertTag =0;
+        [Utilities alertViewMethod:alertString :alertTag];
+    }
+    
+    
+    ////////////// Validation for Password And Confirm Password ///////////////
+    else
+        
+        if (![self.passwordSignUp.text isEqualToString:self.confirmPasswordForSignUp.text])
+        {
+            
+            alertString =@"Password Does Not Match";
+            alertTag =3;
+            [Utilities alertViewMethod:alertString :alertTag];
+            [self.confirmPasswordForSignUp becomeFirstResponder];
+            
+        }
+        else   if (![self.emailSignUp.text isEqualToString:@""])
+        {
+            
+            NSString *emailRegEx = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+            NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegEx];
+            
+            //Validate email address.
+            if (![emailTest evaluateWithObject:self.emailSignUp.text] == YES)
+            {
+                alertString =@"Email is Invalid. Please Enter Valid Email Address.";
+                alertTag=4;
+                [Utilities alertViewMethod:alertString :alertTag];
+                
+            }
+            
+            else
+            {
+                
+                self.nameSignUp.text =[self.nameSignUp.text stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceCharacterSet]];
+                self.passwordSignUp.text=[self.passwordSignUp.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+                self.emailSignUp.text=[self.emailSignUp.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+                
+                testObject = [PFObject objectWithClassName:@"ParseAppDB"];   // Saving the data to the database with ClassName
+                testObject[@"name"] = self.nameSignUp.text;
+                testObject[@"password"]=self.passwordSignUp.text;
+                testObject[@"email"]=self.emailSignUp.text;
+                if (![Utilities CheckInternetConnection])
+                {
+                    
+                    [testObject saveEventually];
+                    UIAlertView *CheckInternetConnection=[[UIAlertView alloc]initWithTitle:@"" message:@"No Internet Connection Available. Your data will be saved after you connect to the internet." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    [CheckInternetConnection show];
+                    CheckInternetConnection=nil;
+                    
+                }
+                
+                else
+                {
+                    [testObject saveInBackground];
+                    
+                    /////////////// Alert to show Successfull Registeration /////////////////
+                    
+                    UIAlertView *loginALert=[[UIAlertView alloc]initWithTitle:@"" message:@"Your New Account Has Been Registered." delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+                    [loginALert show];
+                    loginALert.tag =1;
+                    HUD.hidden=YES;
+                    
+                }
+            }
+        }
+    
+    //  }];
+}
 
+
+//////////////////// Clicking the Button on AlertBox///////////////////////
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    
+    if (alertView.tag==1)
+    {
+        if (buttonIndex==0) // On Clicking OK navigates to previous screen
+        {
+            [self.navigationController popViewControllerAnimated:YES];
+        }
+    }
+    
+}
+#pragma mark - Textfield delegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
 @end
